3a7bc2a10d62da4bf567ad66de6498457469715b
ishahingmire2004-lab <ishahingmire2004@gmail.com>
2026-09-11 08:38:39 +0530

    Create aes_256_gcm.py

---
+++ Task-05-Cryptography/Task-05-Cryptography/aes_256_gcm.py
@@ -0,0 +1,14 @@
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+import os
+
+key = AESGCM.generate_key(bit_length=256)
+aesgcm = AESGCM(key)
+nonce = os.urandom(12)
+plaintext = b"RabTech Academy - Secret Data"
+
+ciphertext = aesgcm.encrypt(nonce, plaintext, None)
+decrypted = aesgcm.decrypt(nonce, ciphertext, None)
+
+print(f"Key: {key.hex()}")
+print(f"Encrypted: {ciphertext.hex()}")
+print(f"Decrypted: {decrypted.decode()}")
